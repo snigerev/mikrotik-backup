cdc938ee1c24773fcacf8197632991f1c2a231c3
Вячеслав Снигерев <v.snigerev@gmail.com>
2024-10-07 06:37:25 +0000

    Security fix and UP version for nodejs

--- server/app/config/env.js
+++ server/app/config/env.js
@@ -2,11 +2,6 @@
 const env = {
     secret: 'bevklwebjvlvh945nebvlh2;1-04uf0n4fok;weirhf902',
     tz: process.env.TZ,
-    // git_username: process.env.GIT_USERNAME,
-    // git_password: process.env.GIT_PASSWORD,
-    // git_path: process.env.GIT_PATH,
-    // git_fullname: process.env.GIT_FULL_NAME,
-    // git_email: process.env.GIT_EMAIL
 };
 
 module.exports = env;
